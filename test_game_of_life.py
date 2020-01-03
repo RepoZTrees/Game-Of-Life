c0b6f1d0e6344556e02ab8f6d66eaf8c832d99bc
Vik <csimm3330.com>
2020-01-03 11:34:34 +0530

    def test_initialize_grid(): Test passed.

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,9 +1,18 @@
+# Test file for Game of Life.
+
 import game_of_life
 
-def test_initialize_grid():
-    grid_actual_value = initialize_grid()
-    grid_expected_value = [[0,0,0,
-                   0,0,0,
-                   0,0,0]]
-    assert grid_actual_value == grid_expected_value
+def test_display_grid():
+
+    expected_grid_value = """. * .
+. * .
+. * ."""
     
+    grid_value = game_of_life.display_grid(grid = [[0,1,0], [0,1,0], [0,1,0]])
+
+    assert game_of_life.display_grid([[0,0,0],[0,0,0],[0,0,0]]) == '. . .\n. . .\n. . .'
+
+    assert game_of_life.display_grid([[0,1],[1,0]]) == '. *\n* .'
+        
+    assert grid_value == expected_grid_value
+
