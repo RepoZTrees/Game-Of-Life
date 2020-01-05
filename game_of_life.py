bdfa510598517d014434164837e76ee9d6e38f50
Vik <csimm3330.com>
2020-01-05 10:30:16 +0530

    added count_live_neighbours().

--- game_of_life.py
+++ game_of_life.py
@@ -16,3 +16,25 @@ def display_grid(grid):
     return "\n".join(db)
 
 #print(display_grid())
+
+#--------
+grid = [[0,1,0],
+        [0,1,0],
+        [0,1,0]]
+
+def count_live_neighbours(grid, r, c):
+    def get(r, c):
+        if 0 <= r < len(grid) and 0 <= c < len(grid[r]):
+            return grid[r][c]
+        else:
+            return 0
+
+    neighbors_list = [get(r-1, c-1), get(r-1, c), get(r-1, c+1),
+                      get(r  , c-1),              get(r  , c+1),
+                      get(r+1, c-1), get(r+1, c), get(r+1, c+1)]
+
+    return sum(map(bool, neighbors_list))
+
+#a = count_live_neighbours(grid,0,1)
+#print(a)
+
