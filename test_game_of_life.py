571b14cc2c166aa6db5be569830991ffffdb18e5
Vik <csimm3330.com>
2020-01-06 13:29:09 +0530

    added test_live_cell_no_niehghbours_dies(). Test failed.

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -16,13 +16,13 @@ def test_display_grid():
         
     assert grid_value == expected_grid_value
 
-
+    
 def test_count_live_neighbours():
 # count_live_neighbours(grid,r,c). So (0,2 is row0, column2)
 
     assert game_of_life.count_live_neighbours([[0,1,0],
                                                [0,1,0],
-                                               [0,1,0]], (0),(2)) == 2
+                                               [0,1,0]], (1),(0)) == 3
 
     assert game_of_life.count_live_neighbours([[0,1,1],
                                                [0,1,0],
@@ -31,3 +31,55 @@ def test_count_live_neighbours():
     assert game_of_life.count_live_neighbours([[0,1],
                                                [0,1,0]], (0),(0)) == 2
                                                                                                            
+
+def test_live_cell_no_neighbours_dies():
+    assert game_of_life.dead_or_alive([[0,0,0],
+                                       [0,0,0],
+                                       [0,1,0]], (2),(1)) == 0
+
+
+"""
+#select all, alt ;
+
+def test_live_cell_one_neighbours_dies():
+    assert game_of_life.dead_or_alive([[1,1,0],
+                                       [0,0,0],
+                                       [0,1,0]], (0),(0)) == 0
+
+    
+def test_live_cell_two_neighbours_live():
+    assert game_of_life.dead_or_alive([[1,1,0],
+                                       [0,1,0],
+                                       [0,1,0]],(0),(0)) == 1 
+
+def test_live_cell_three_neighbours_live():
+    assert game_of_life.dead_or_alive([[0,1,0],
+                                       [1,1,0],
+                                       [0,1,0]],(1),(0)) == 1
+
+
+def test_live_cell_more_than_three_neighbours_dies():
+    assert game_of_life.dead_or_alive([[1,1,0],
+                                       [1,1,0],
+                                       [0,1,0]],(1),(1)) == 0
+
+
+def test_dead_cell_two_neighbours_remains_dead():
+    assert game_of_life.dead_or_alive([[0,1,0],
+                                       [0,1,0],
+                                       [1,1,0]],(0),(0)) == 0
+
+
+def test_dead_cell_three_neighbours_realive():
+    assert game_of_life.dead_or_alive([[0,1,0],
+                                       [0,1,0],
+                                       [0,1,0]],(1),(0)) == 1
+
+
+def test_dead_cell_more_than_three_neighbours_remains_dead():
+    assert game_of_life.dead_or_alive([[1,1,0],
+                                       [0,1,0],
+                                       [1,1,0]],(1),(0)) == 0
+
+"""
+
