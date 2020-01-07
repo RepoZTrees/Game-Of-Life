5b94fed240aeedc45d336e85af3687d1cfcb29df
Vik <csimm3330.com>
2020-01-07 16:18:28 +0530

    added main(). Final version.

--- game_of_life.py
+++ game_of_life.py
@@ -1,4 +1,5 @@
 # Game of life.
+from copy import deepcopy
 
 def display_grid(grid):
     da = []
@@ -15,13 +16,6 @@ def display_grid(grid):
     
     return "\n".join(db)
 
-#print(display_grid())
-
-#--------
-
-grid = [[0,0,0],
-        [0,0,0],
-        [0,1,0]]
 
 def count_live_neighbours(grid, r, c):
     def get(r, c):
@@ -34,26 +28,77 @@ def count_live_neighbours(grid, r, c):
                       get(r  , c-1),              get(r  , c+1),
                       get(r+1, c-1), get(r+1, c), get(r+1, c+1)]
 
-    return sum(map(bool, neighbors_list))
+    return sum(neighbors_list)
 
-#a = count_live_neighbours(grid,1,1)
+#a = count_live_neighbours(grid,1,2)
 #print(a)
 
-#------
 
 def dead_or_alive(grid,r,c):
     neighbours_count = count_live_neighbours(grid,r,c)
 
-    if grid[r][c] == 1:
-        if neighbours_count < 2 or neighbours_count > 3:
-            return 0
-        else:
-            return 1
-    else:
+    if grid[r][c] == 0:
         if neighbours_count == 3:
             return 1
         else:
             return 0
+    else:
+        if neighbours_count <2 or neighbours_count >3:
+            return 0
+        else:
+            return 1
 
-#b = dead_or_alive(grid,2,1)
-#print(b)
+# b = dead_or_alive(grid,1,2)
+# print(b)
+
+
+def value_of_next_generation_grid(grid):
+    next_gen_grid = deepcopy(grid)
+    number_of_rows = len(grid)
+    number_of_columns = len(grid[0])
+    
+    for rows in range(0, number_of_rows):
+        for columns in range(0, number_of_columns):
+            next_gen_grid[rows][columns] = dead_or_alive(grid,rows,columns)
+    return next_gen_grid
+            
+# xz = value_of_next_generation_grid(grid)
+# print(xz)
+#--------
+
+def main():
+    grid = [[0,0,0],
+            [1,1,1],
+            [0,0,0]]
+    
+    for i in range(5):
+        print(display_grid(grid))
+        print('\n')
+        grid = value_of_next_generation_grid(grid)        
+
+main()
+
+#------
+
+# import pdb; pdb.set_trace() #is an interactive source code debugger for Python programs.
+# from copy import deepcopy:
+"""
+For example:
+
+>>> x = [1,2,3]
+>>> y = x
+>>> print(x,y)
+[1, 2, 3] [1, 2, 3]
+>>> y.append(4)
+>>> print(x,y)
+[1, 2, 3, 4] [1, 2, 3, 4]
+>>> y.append(5)
+>>> print(x,y)
+[1, 2, 3, 4, 5] [1, 2, 3, 4, 5]
+>>> import copy
+>>> y = copy.deepcopy(x)
+>>> y.append(6)
+>>> print(x,y)
+[1, 2, 3, 4, 5] [1, 2, 3, 4, 5, 6]
+
+"""
